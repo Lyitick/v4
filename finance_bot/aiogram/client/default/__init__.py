810db7c47ac99bb0b0c9cee513c77665c4d3a5a5
LyiTicko <99051490+Lyitick@users.noreply.github.com>
2025-12-04 22:44:30 +0300

    Delete finance_bot/aiogram directory

--- finance_bot/aiogram/client/default/__init__.py
+++
@@ -1,3 +0,0 @@
-class DefaultBotProperties:
-    def __init__(self, parse_mode: str | None = None) -> None:
-        self.parse_mode = parse_mode
--- finance_bot/aiogram/fsm/__init__.py
+++
@@ -1,2 +0,0 @@
-from .context import FSMContext  # noqa: F401
-from .state import State, StatesGroup  # noqa: F401
